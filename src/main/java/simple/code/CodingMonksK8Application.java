a633a4390a9df427ae2ee4cf882be1c12a41de27
CodingMonks <tech4saurabh@gmail.com>
2021-03-11 11:27:33 -0500

    Logs added

--- src/main/java/simple/code/CodingMonksK8Application.java
+++ src/main/java/simple/code/CodingMonksK8Application.java
@@ -1,13 +1,34 @@
 package simple.code;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.Date;
+
 
 @SpringBootApplication
 public class CodingMonksK8Application {
 
+	private final Logger logger = LogManager.getLogger(CodingMonksK8Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(CodingMonksK8Application.class, args);
+	}
+
+	@PostConstruct
+	public void AfterConstruction()
+	{
+		logger.info("AfterConstruction "+new Date());
+	}
+
+	@PreDestroy
+	public void PreDestroyImpl()
+	{
+
+		logger.info("PreDestroyImpl "+new Date());
 	}
 
 }
--- src/main/java/simple/code/controller/MyRestController.java
+++ src/main/java/simple/code/controller/MyRestController.java
@@ -1,24 +1,33 @@
 package simple.code.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import simple.code.CodingMonksK8Application;
 
 import java.util.Date;
 
 @RequestMapping("/kube")
 @RestController
 public class MyRestController {
+    private final Logger logger = LogManager.getLogger(MyRestController.class);
 
     @GetMapping("/method1")
     public String myMethod()
     {
-        return String.format("Hello from method1 @ "+new Date());
+        Date date = new Date();
+        logger.info("myMethod1 "+date);
+
+        return String.format("Hello from method1 @ "+date);
     }
 
     @GetMapping("/method2")
     public String myMethod2()
     {
-        return String.format("Hello from method2 @ "+new Date());
+        Date date = new Date();
+        logger.info("myMethod2 "+date);
+        return String.format("Hello from method2 @ "+date);
     }
 }
